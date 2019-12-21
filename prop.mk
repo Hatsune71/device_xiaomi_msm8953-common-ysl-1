b37ed402402fe248a091c8102ea776a7b6d0d966
chandra <chandra.chaganti@hotmail.com>
2019-12-21 14:31:33 +0530

    msm8953-common: Use AOSP SurfaceFlinger
     * AOSP surfaceflinger has more smoother UI than that of CAF
    
    Signed-off-by: itsvixano <vixanonew@gmail.com>

--- prop.mk
+++ prop.mk
@@ -99,6 +99,13 @@ debug.sf.recomputecrop=0 \
 persist.hwc.ptor.enable=true \
 debug.sf.gpu_comp_tiling=1
 
+# SurfaceFlinger
+ro.surface_flinger.protected_contents=true
+ro.surface_flinger.use_smart_90_for_video=true
+ro.surface_flinger.set_display_power_timer_ms=10000
+ro.surface_flinger.set_touch_timer_ms=5000
+ro.surface_flinger.set_idle_timer_ms=9000
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
 drm.service.enabled=true
