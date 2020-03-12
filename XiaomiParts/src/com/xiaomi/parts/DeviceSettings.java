97563a814c13183590474b4b9f1d5d47fb8344a8
Genkzsz11 <Genkzsz11@gmail.com>
2020-03-12 14:10:55 +0530

    msm8953-common: Update Xiaomi Parts

--- XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
+++ XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
@@ -51,8 +51,8 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String CATEGORY_FASTCHARGE = "usb_fastcharge";
     public static final String PREF_USB_FASTCHARGE = "fastcharge";
     public static final String USB_FASTCHARGE_PATH = "/sys/kernel/fast_charge/force_fast_charge";
-
     private SecureSettingSwitchPreference mFastcharge;
+
     //public static final String PREF_TORCH_BRIGHTNESS = "torch_brightness";
     //private static final String TORCH_1_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
     //        "spmi/spmi-0/spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_0/max_brightness";
